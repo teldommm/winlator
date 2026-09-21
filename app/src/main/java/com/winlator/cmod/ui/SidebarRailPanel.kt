36c46328ce13c31716c27a325cd95c71ec9b1b70
teldommm <teldom14@gmail.com>
2026-09-22 00:41:31 +0300

    fix

--- app/src/main/java/com/winlator/cmod/ui/SidebarRailPanel.kt
+++ app/src/main/java/com/winlator/cmod/ui/SidebarRailPanel.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
