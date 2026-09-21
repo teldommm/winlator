2b008698c78c801d1a304aecc62d3f8565b430b5
teldommm <teldom14@gmail.com>
2026-09-22 01:18:13 +0300

    fix

--- app/src/main/java/com/winlator/cmod/ui/container/ContainerEditorV2.kt
+++ app/src/main/java/com/winlator/cmod/ui/container/ContainerEditorV2.kt
@@ -42,6 +42,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.produceState
