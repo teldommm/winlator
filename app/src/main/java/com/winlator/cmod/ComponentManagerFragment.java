ab092487e2343a928f353734fec70e41ed8514ea
teldommm <teldom14@gmail.com>
2026-09-22 13:51:02 +0300

    fix

--- app/src/main/java/com/winlator/cmod/ComponentManagerFragment.java
+++ app/src/main/java/com/winlator/cmod/ComponentManagerFragment.java
@@ -138,7 +138,7 @@ public class ComponentManagerFragment extends Fragment {
 
         FrameLayout root = new FrameLayout(requireContext());
         ComposeView composeView = new ComposeView(requireContext());
-        composeView.setViewCompositionStrategy(ViewCompositionStrategy.Companion.getDisposeOnViewTreeLifecycleDestroyed());
+        composeView.setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed.INSTANCE);
         root.addView(composeView, new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT));
 
         composeController = OnboardingComposeHost.attachToView(
