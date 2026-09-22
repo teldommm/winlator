93037be626fedbe7c7b171a179c35c8bca9569d0
teldommm <teldom14@gmail.com>
2026-09-22 23:41:43 +0300

    fix

--- app/src/main/java/com/winlator/cmod/ui/shortcut/ShortcutEditorV2.kt
+++ app/src/main/java/com/winlator/cmod/ui/shortcut/ShortcutEditorV2.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
